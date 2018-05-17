57d38a4102176e82af8dcd804b8ac7bfe03d88de
Nate Denys <nate_denys@yahoo.com>
2018-05-17 13:28:20 -0700

    Located errors
    Commented application to assess errors and fixed app

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
-todoList() {
-	username = document.getElementById("username").value
-	var comment = document.getElementById("comment").value
+// no "function" keyword was used to identify and create the function
 
-	var userNameText = document.createTextNode("Username: " + username)
-	commentText = document.createTextNode("Comment: " + comment)
+function todoList() {
+    // no "var" keyword before username
+    var username = document.getElementById("username").value;
+    var comment = document.getElementById("comment").value;
 
-	var newListItem = document.createelement("LI")
-	newListItem.className = "list-group-item"
-	var newUserName = document.createElement("H3")
-	newUserName.appendChild(userNameText)
-	var newComment = document.createElement("P")
-	newComment.appendChild(commentText)
+    var userNameText = document.createTextNode("Username: " + username)
+        // no "var" keyword before commentText
+    var commentText = document.createTextNode("Comment: " + comment)
 
-	newListItem.appendChild(newUserName)
-	newListItem.appendChild(newComment)
-	document.getElementById("commentList").appendChild(newListItem)
+    // camelCase createElement
+    var newListItem = document.createElement("LI")
+    newListItem.className = "list-group-item"
+    var newUserName = document.createElement("H3")
+    newUserName.appendChild(userNameText)
+    var newComment = document.createElement("P")
+    newComment.appendChild(commentText)
+
+    newListItem.appendChild(newUserName)
+    newListItem.appendChild(newComment)
+    document.getElementById("commentList").appendChild(newListItem)
 }
